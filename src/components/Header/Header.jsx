47ec00c34c59a3e84a18145c6e33e0aaf42f2e76
isfhan <isfhan729@gmail.com>
2022-03-26 20:14:22 +0500

    Change in css add icons change  Link

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,32 +1,38 @@
-import { Container, Navbar, Nav, NavDropdown } from "react-bootstrap";
+import { Container, Navbar, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import './Header.css'
+import { FaShoppingCart } from 'react-icons/fa';
 
 function Header() {
-    return (
-        <>
-            <Navbar collapseOnSelect expand="lg" className="isf-navbar" variant="dark">
+  return (
+    <>
+      <Navbar collapseOnSelect expand="lg" className="isf-navbar" variant="dark">
+        <Container>
+          <Navbar.Brand as={Link} to="/" >
+            E-STORE 🌎
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to="/product-listing">Products</Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Link as={Link} to="/cart">
                 <Container>
-                    <Navbar.Brand as={Link} to="/" >E-STORE</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                    <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className="me-auto">
-                            <Nav.Link as={Link} to="/" >Home</Nav.Link>
-                            <Nav.Link as={Link} to="/shop" >Shop</Nav.Link>
-                        </Nav>
-                        <Nav>
-                            <Nav.Link  as={Link} to="/cart">Cart 🛒</Nav.Link>
-                            {/* <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                  <FaShoppingCart /> {0}
+                </Container>
+              </Nav.Link>
+              {/* <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
                                 <NavDropdown.Item as={Link} to="/action" >Action</NavDropdown.Item>
                                 <NavDropdown.Item  as={Link} to="/action/2" >Another action</NavDropdown.Item>
                             </NavDropdown> */}
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
 
-        </>
-    )
+    </>
+  )
 }
 
 export default Header;
