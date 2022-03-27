932beaee53012b226d4fc3a199f8f3b56073c424
isfhan <isfhan729@gmail.com>
2022-03-27 19:58:08 +0500

    Fix some style issues

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,7 +1,7 @@
 import { Container, Row, Col, Card, Button } from "react-bootstrap";
 import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
-import { FaChevronCircleRight } from "react-icons/fa";
+import { FaArrowRight } from "react-icons/fa";
 import { useHistory } from "react-router-dom";
 import { useSelector } from "react-redux";
 import Image from "react-bootstrap/Image";
@@ -24,16 +24,16 @@ function Landing() {
       <Col sm={12} md={6} lg={4} key={"featuredProducts" + index} className="mb-3" >
         <Card className="isf-card">
           <Card.Img variant="top" src={product.image} className="isf-card-image" />
-          <Card.Body className="isf-card-body">
+          <Card.Body className="isf-card-body d-flex flex-column justify-content-between">
             <Card.Title style={{ fontWeight: '700' }}>
               {product.title}
             </Card.Title>
             <Card.Text>
               {product.description.slice(0, 100)}...
             </Card.Text>
-                <Button variant="primary" className=" align-items-center gap-3" onClick={() => viewProductDetail(product.id)}>
-                  View detail <FaChevronCircleRight />
-                </Button>
+            <Button variant="primary" className="d-flex align-items-center gap-3 justify-content-center" onClick={() => viewProductDetail(product.id)}>
+              View detail <FaArrowRight />
+            </Button>
           </Card.Body>
         </Card>
       </Col>
@@ -44,7 +44,7 @@ function Landing() {
     <>
       <Header />
       <Image src={banner} alt="Banner" fluid />
-      <Container  className="isf-container-height">
+      <Container className="isf-container-height">
         <h1 className="text-center mb-4 mt-4">Featured Product</h1>
         <Row className="pt-4 pb-4">
           {featuredProducts()}
@@ -52,7 +52,7 @@ function Landing() {
         <Row className="pt-4 pb-4">
           <Col className="d-flex justify-content-center">
             <Button variant="success" size="lg" className="d-flex align-items-center gap-3">
-              See all products <FaChevronCircleRight />
+              See all products <FaArrowRight />
             </Button>
           </Col>
         </Row>
--- src/pages/ProductListing/ProductListing.jsx
+++ src/pages/ProductListing/ProductListing.jsx
@@ -1,5 +1,5 @@
 import { Container, Row, Col, Card, Button } from "react-bootstrap";
-import { FaChevronCircleRight } from "react-icons/fa";
+import { FaArrowRight } from "react-icons/fa";
 import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
 import { useHistory } from "react-router-dom";
@@ -20,15 +20,15 @@ function ProductListing() {
       <Col sm={12} md={6} lg={4} key={"featuredProducts" + index} className="mb-3" > 
         <Card className="isf-card">
           <Card.Img variant="top" src={product.image} className="isf-card-image" />
-          <Card.Body className="isf-card-body">
+          <Card.Body className="isf-card-body d-flex flex-column justify-content-between">
             <Card.Title style={{ fontWeight: '700' }}>
               {product.title}
             </Card.Title>
             <Card.Text>
               {product.description.slice(0, 100)}...
             </Card.Text>
-            <Button variant="primary" onClick={() => viewProductDetail(product.id)}>
-              View detail <FaChevronCircleRight />
+            <Button variant="primary" className="d-flex align-items-center gap-3 justify-content-center" onClick={() => viewProductDetail(product.id)}>
+              View detail <FaArrowRight />
             </Button>
           </Card.Body>
         </Card>
